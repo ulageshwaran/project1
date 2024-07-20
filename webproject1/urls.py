016e5daebaf1c060bdd792a12afe76e34559a546
Ulageshwaran <ulageshofficial@gmail.com>
2024-07-20 10:26:44 +0530

    second commit

--- webproject1/urls.py
+++ webproject1/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path("",views.index),
+    path("blog",views.blog),
+    path("cart",views.cart),
+    path("category",views.category),
+    path("checkout",views.checkout),
+    path("confirmation",views.confirmation),
+    path("contact",views.contact),
+    path("elements",views.elements),
+    path("login",views.login),
+    path("tracking",views.tracking),
+    path("single-blog",views.single_blog),
+    path("single-product",views.single_product),
 ]
--- webproject1/views.py
+++ webproject1/views.py
@@ -4,3 +4,36 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def cart(request):
+    return render(request, "cart.html")
+
+def category(request):
+    return render(request, "category.html")
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+def confirmation(request):
+    return render(request, "confirmation.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def elements(request):
+    return render(request, "elements.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def tracking(request):
+    return render(request, "tracking.html")
+
+def single_blog(request):
+    return render(request, "single-blog.html")
+
+def single_product(request):
+    return render(request, "single-blog.html")
